6aecc039aaffc6e116ad0ea32b6c43a268664c7e
vin-droid <vineet.12cs069@abes.ac.in>
2016-12-27 08:44:34 +0530

    basic folder stucturing done

---
+++ app/scripts/controllers/eventCtrl.js
@@ -0,0 +1,11 @@
+
+    app.controller('eventCtrl', function($scope){
+    	$scope.eventlist = [
+           {name:'event1',duration: '6'},
+           {name:'event2',duration: '6'},
+           {name:'event3',duration: '6'},
+           {name:'event4',duration: '6'},
+           {name:'event5',duration: '6'}
+    	];
+    	
+    }) ;        
